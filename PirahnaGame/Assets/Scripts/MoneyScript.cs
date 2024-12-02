f388d954c1dc9ae6dfae3f4d0e97661fafa8fb92
Michael Bennett <msbennet@millersville.edu>
2024-12-02 10:04:27 -0500

    Barebones money script is added

---
+++ PirahnaGame/Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyScript : MonoBehaviour
+{
+    private int totalCoins = 0;
+    
+ void addToBalance(){
+    if(totalCoins = 0){
+
+    }
+    if(totalCoins = 0) {
+
+    }
+    if(totalCoins = 0){
+
+    } 
+    if(totalCoins = 0){
+
+    }
+    if(totalCoins = 0){
+
+    }
+    
+}
+
+void subBalance(){
+
+}
+
+void getBalance(){
+    return totalCoins;
+}
+
+}
